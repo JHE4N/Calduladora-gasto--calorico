19edfdfb59166cce5f7acf3abcece28f0e9c560f
Jhean Bryan <jheanbeyan123@gmail.com>
2022-05-31 19:56:47 -0400

    create new project

---
+++ app.js
@@ -0,0 +1,16 @@
+//peso, altura, idade, sexo
+/*
+Fórmula para homens: TMB = fator da taxa de atividade x {66 + [(13,7 x Peso(kg)) + ( 5 x Altura(cm)) – (6,8 x Idade(anos))]}
+Fórmula para mulheres: TMB = fator da taxa de atividade x {655 + [(9,6 x Peso(kg)) + (1,8 x Altura(cm)) – (4,7 x Idade(anos))]}
+*/
+let peso = NaN
+let altura = NaN
+let idade = NaN
+let sexo = NaN
+let tmb = NaN
+if(sexo == 'M'){
+    tmb = 66 + (13.7 * peso) + (5 * altura) - (6.8 * idade)
+} else {
+    tmb = 655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade)
+}
+console.log(tmb)
